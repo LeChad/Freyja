e61741199734e0beb936a5a1e2c379bbda439a61
Chadwick <cwick.colvin@gmail.com>
2023-06-11 10:47:22 -0700

    Added password reset functionality and templates.

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from .views import (
-    homepage
+    homepage,
+    test_page
 )
 
 urlpatterns = [
     path('', homepage, name='home'),
-
+    path('test', test_page, name='test')
    # path('', TemplateView.as_view(template_name="registration/logout_success.html"), name='logout_success')
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login
+from django.core.mail import send_mail
 from .forms import NewUserForm
+
 
 def homepage(request):
     context = {}
+
     return render(request, 'main/index.html', context)
 
+
+def test_page(request):
+    context = {}
+    if request.method == "POST":
+        send_mail(
+            subject="Welcome to Freyja!",
+            message=f"""Hello, test!
+            
+            Welcome to Project Freyja! A album and photography web application made for NAU class PLD450.
+            Your registered username is, test. Please feel free to explore the project and provide
+            any feedback. 
+
+            Best Regards, 
+            Freyja Team
+            
+            """,
+            from_email="project.pld450@gmail.com",
+            recipient_list=["lechadify@gmail.com"]
+        )
+        messages.info(request, "Sent message?")
+    return render(request, 'main/test_page.html', context)
 
 def sign_up(request):
     context = {}
@@ -19,6 +43,22 @@ def sign_up(request):
             if checkbox_value == "on":
                 form.save()
                 messages.success(request, f"Your account has been created.")
+                send_mail(
+                    subject="Welcome to Freyja!",
+                    message=f"""
+                        Hello, {request.POST.get('first_name')}!
+                        
+                        Welcome to Project Freyja! A album and photography web application made for NAU class PLD450.
+                        Your registered username is, {request.POST.get('username')}. Please feel free to explore the project and provide
+                        any feedback. 
+                        
+                        Best Regards, 
+                        Freyja Team
+                    """,
+                    from_email="project.pld450@gmail.com",
+                    recipient_list=[request.POST.get("email")]
+
+                )
                 return redirect('login')
             else:
                 messages.error(request, "To create an account, please agree to our terms and conditions.")
